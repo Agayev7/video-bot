c51d37211512126a4313dcaf2d1d02ea589eec5c
Agayev7 <otaboyevogabek72@gmail.com>
2025-09-21 09:14:47 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,23 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 import os
+from dotenv import load_dotenv
 
-TOKEN = os.getenv("TOKEN")  # .env fayldan TOKEN
+load_dotenv()  # .env faylni o‘qiydi
+TOKEN = os.getenv("TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Salom! Bot ishga tushdi ✅")
 
 def main():
+    # Eski Updater ishlatilmaydi
     app = Application.builder().token(TOKEN).build()
+    
+    # Handler qo‘shamiz
     app.add_handler(CommandHandler("start", start))
+    
+    # Botni ishga tushuramiz
     app.run_polling()  # eski asyncio kerak emas
 
 if __name__ == "__main__":
     main()
-
